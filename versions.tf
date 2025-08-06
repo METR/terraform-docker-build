3338601bcbb4a7ec57f9069051b1f8b00a741f95
Mark Ballew <mark.ballew@metr.org>
2025-08-05 21:02:19 -0700

    version like other modules do

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.99.0"
+      version = "~> 5.99"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.4"
+      version = "~> 3.2"
     }
   }
 }
